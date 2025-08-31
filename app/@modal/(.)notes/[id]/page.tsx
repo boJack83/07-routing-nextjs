c23c6a5a9c550094c689c58c2f29d1c3716685fa
Serhii Kravcnenko <sergkrava1983@gmail.com>
2025-08-31 22:13:04 +0300

    fix name /NotePreview.client.tsx

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NotePreview from "./NotePreview"
+import NotePreview from "./NotePreview.client"
 
 type Props = {
   params: Promise<{ id: string }>;
